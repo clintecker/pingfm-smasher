abcac8a9aa66d97689e001b3a20cf1ec7490a4a1
Clinton Ecker <clint@arstechnica.com>
2009-02-21 18:10:24 -0600

    Committing a real README and a setup.py install

---
+++ setup.py
@@ -0,0 +1,17 @@
+from distutils.core import setup
+
+setup(name='pingfmsmash',
+      version='1.0',
+      description='Combine RSS feeds into Ping.FM',
+      author='Clint Ecker',
+      author_email='clint@arstechnica.com',
+      url='https://github.com/ArsTechnica/pingfm-smasherr/tree/master',
+      packages=['pingfmsmash', 'pingfmsmash.management', 'pingfmsmash.management.commands'],
+      classifiers=['Development Status :: 4 - Beta',
+                   'Environment :: Web Environment',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: BSD License',
+                   'Operating System :: OS Independent',
+                   'Programming Language :: Python',
+                   'Topic :: Utilities'],
+      )
